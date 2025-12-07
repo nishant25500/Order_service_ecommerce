64c70a77f20ae63f24113d1b719c0b3b0022ea85
nishant <nishant.20235@knit.ac.in>
2025-12-07 20:58:36 +0530

    initial setup

---
+++ src/main/java/com/mishri/order_serive_ecommerce/OrderSeriveEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.mishri.order_serive_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSeriveEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSeriveEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mishri/order_serive_ecommerce/OrderSeriveEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mishri.order_serive_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSeriveEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
